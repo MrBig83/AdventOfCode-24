f8bfec4e6a88911f3e9b2ae7406dc7ebb1fa8e4e
MrBig83 <mjohansson176@gmail.com>
2024-12-01 20:47:31 +0100

    Cleanup

--- 01.js
+++ 01.js
@@ -1,14 +1,11 @@
-
 const fs = require('fs')
-
 
 fs.readFile("01_input.txt", (err, input) => {
     if (err) throw err; 
     else {
 
         let listan = input.toString()
-        // let array = input.split("   " || "\n")
-        // Split the data into rows and process into two separate arrays
+
         const rows = listan.trim().split('\n');
 
         const leftColumn = [];
@@ -23,42 +20,19 @@ fs.readFile("01_input.txt", (err, input) => {
 
         leftColumn.sort()
         rightColumn.sort()
-        // console.log('Left Column:', leftColumn[2]);
-        // console.log('Right Column:', rightColumn[2]);
         
         for (let i = 0; i < leftColumn.length; i++) {
-        // for (let i = 0; i < 3; i++) {
             const a = leftColumn[i];
             const b = rightColumn[i];
             let sum = a-b;
             distanceArray.push(Math.abs(sum)) 
         }
-        
-   
-        console.log(distanceArray);
+            
+        let totalSum = 0;
+        for (let i = 0; i < distanceArray.length; i++) {
+        totalSum += distanceArray[i];
+        }
 
-
-
-        
-let totalSum = 0;
-for (let i = 0; i < distanceArray.length; i++) {
-  totalSum += distanceArray[i];
-}
-
-console.log(totalSum);
-
-        
-        
-                
-        
+        console.log(totalSum)     
     }
 })
-
-
-// async function readContent() {
-//     fetch('01_input.txt')
-//     .then(response => response.text())
-//     .then(text => console.log(text))
-// }
-
-// readContent()
